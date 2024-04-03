d848f50b2f699e42bb02530f904d2f57c3230d69
mikey <mikey@hey.com>
2024-04-03 16:05:11 -0500

    use env variable for port

--- app.js
+++ app.js
@@ -35,8 +35,8 @@ const models = require('./db/models');
 // import controllers
 require('./controllers/events')(app, models);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3030;
 
 app.listen(port, () => {
-  console.log('App listening on port 3000');
+  console.log(`server started on port ${port}`);
 });
